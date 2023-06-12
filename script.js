2567eb1b6188c7a8ff1a921eae747e8f1f100828
Sergio Luiz Falcetti <falcettijr@gmail.com>
2023-06-12 00:45:03 -0300

    responsividade menu hamburguer

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let button = document.getElementById("button");
+let fechar = document.getElementById("close")
+
 button.addEventListener("click", function(){
     document.getElementById("menu").style.left=0;
-})
+});
+
+fechar.addEventListener("click", function(){
+    document.getElementById("menu").style.left="100%";
+});
